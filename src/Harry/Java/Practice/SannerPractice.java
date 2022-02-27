b695dab698025a73841082a47ae4c50676c2c249
Sumit Dagi <sumeet.dagi@gmail.comm>
2022-02-27 07:38:45 -0600

    class created

---
+++ src/Harry/Java/Practice/SannerPractice.java
@@ -0,0 +1,14 @@
+package Harry.Java.Practice;
+
+public class SannerPractice {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        int salary;
+        System.out.println("My name is sumeet");
+
+
+
+    }
+
+}
