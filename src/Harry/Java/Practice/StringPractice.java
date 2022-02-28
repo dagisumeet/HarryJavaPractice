41961c34d3df308db0a039f0d1cc733cef0a7a56
Sumit Dagi <sumeet.dagi@gmail.comm>
2022-02-28 05:01:48 -0600

    more practice

---
+++ src/Harry/Java/Practice/StringPractice.java
@@ -0,0 +1,14 @@
+package Harry.Java.Practice;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+        int v=5;
+        int u=4;
+        int a = 1;
+        int s= 2;
+
+        int answer= ((v*v-u*u)/2*a*s);
+        System.out.println(answer);
+    }
+}
